994f44c7f0668313e3026ce1e4b715935cd0366a
Zakir Mustafa Kemal Kalayci <118366212+ZakirMK@users.noreply.github.com>
2024-11-21 12:41:38 +0300

    add to cart process test added

--- cypress/e2e/features/sauceDemo.feature
+++ cypress/e2e/features/sauceDemo.feature
@@ -9,3 +9,9 @@ Feature: Sauce Demo Tests
     Given I visit the Sauce Demo page
     When I log in
     Then I should see the home page products and their prices
+
+  Scenario: Check add to cart process
+    Given I visit the Sauce Demo page
+    When I log in
+    And I add product 1 to the cart
+    Then the cart should show the correct product 1 details
--- cypress/e2e/step_definitions/sauceDemo.steps.ts
+++ cypress/e2e/step_definitions/sauceDemo.steps.ts
@@ -11,10 +11,19 @@ When('I log in', () => {
     sauceDemo.login()
 });
 
+When('I add product {int} to the cart', (productNumber: number) => {
+    sauceDemo.cartIsEmpty()
+    sauceDemo.addToCart(productNumber)
+});
+
 Then('I should see the home page', () => {
     sauceDemo.assertHomePage()
 });
 
 Then('I should see the home page products and their prices', () => {
     sauceDemo.checkHomeProductsAndPrices();
+});
+
+Then('the cart should show the correct product {int} details', (productNumber: number) => {
+    sauceDemo.assertItemIsAddedToCart(productNumber)  
 });
